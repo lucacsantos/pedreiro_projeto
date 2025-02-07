ce6572fa6e207ba9fba412ee48ad606eed5e6d13
Lucas Cosme <santoslucascosme12@gmail.com>
2025-02-07 16:28:29 -0300

    Ajustes na tela de login e cadastro

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,14 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [FormsModule, MatButtonModule, MatInputModule, MatFormFieldModule, ReactiveFormsModule],
+  standalone: true,
+  imports: [FormsModule, MatButtonModule, MatInputModule, MatFormFieldModule,CommonModule ,ReactiveFormsModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
@@ -16,22 +18,36 @@ export class CadastroComponent {
   cadastroSchema = new FormGroup({
     nome: new FormControl(""),
     email: new FormControl(""),
-    senha: new FormControl
+    senha: new FormControl(""),
+    confirmarSenha: new FormControl("")
   })
 
   private _snackBar = inject(MatSnackBar);
 
-
-  constructor(private formBuilder: FormBuilder, private router: Router){
+  constructor(private formBuilder: FormBuilder, private router: Router) {
     this.cadastroSchema = this.formBuilder.group({
-      nome: new FormControl(""),
-      email: new FormControl(""),
-      senha: new FormControl("")
-    })
+      nome: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      senha: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      confirmarSenha: new FormControl('', Validators.required)
+    }, { validators: this.senhasIguais });
   }
 
-  onSubimit(){
-    this._snackBar.open("Cadastro realizado com sucesso.", "Fechar");
+  senhasIguais(group: AbstractControl): ValidationErrors | null {
+    const senha = group.get('senha')?.value;
+    const confirmarSenha = group.get('confirmarSenha')?.value;
+    return senha === confirmarSenha ? null : { senhasNaoIguais: true };
+  }
+
+  onSubmit(event: Event): void {
+    event.preventDefault();
+
+    if (this.cadastroSchema.invalid) {
+      this._snackBar.open('Preencha todos os campos corretamente.', 'Fechar');
+    }
+
+    this._snackBar.open('Cadastro realizado com sucesso.', 'Fechar');
     this.router.navigate(['/login']);
   }
+
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,15 @@
 import { Component,inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSnackBar} from '@angular/material/snack-bar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, MatButtonModule, MatInputModule, MatFormFieldModule, ReactiveFormsModule],
+  imports: [FormsModule, MatButtonModule, MatInputModule, MatFormFieldModule,CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -21,19 +22,18 @@ export class LoginComponent {
 
   constructor(private router: Router,  private formBuilder: FormBuilder){
     this.loginSchema = this.formBuilder.group({
-      email: new FormControl(""),
-      senha: new FormControl("")
+      email: new FormControl("", [Validators.required]),
+      senha: new FormControl("", [Validators.required])
     })
   }
 
 
-  onSubimit(){
-    console.log(this.loginSchema.controls.email.value);
+  onSubmit(){
     if(this.loginSchema.controls.email.value !== '' && this.loginSchema.controls.senha.value !== ''){
-      this._snackBar.open("Login feito com sucesso.", "FECHAR");
+      this._snackBar.open("Login feito com sucesso.", "Fechar");
       this.router.navigate(['/home']);
     } else{
-      this._snackBar.open("E-mail ou senha inválida.", "FECHAR");
+      this._snackBar.open("E-mail ou senha inválida.", "Fechar");
     }
   }
 }
